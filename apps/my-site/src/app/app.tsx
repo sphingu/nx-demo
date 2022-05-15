4a8e99089a2b8d85582ed8fe961bef922bdeb5a0
Sumit Hingu <sphingu@pm.me>
2022-05-15 17:03:03 +0530

    Integrated api into my-site app

--- apps/my-site/src/app/app.tsx
+++ apps/my-site/src/app/app.tsx
@@ -1,11 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import { UiLogin, PageTitle } from '@my-org/ui-login';
+import { useEffect, useState } from 'react';
 
 export function App() {
+  const [message, setMessage] = useState<string>('Loading...');
+  useEffect(() => {
+    fetch('/api')
+      .then((res) => res.json())
+      .then((data) => setMessage(data?.message || ''));
+  });
+  console.log('RENDERING');
   return (
     <>
       <h1>Welcome to My-Site</h1>
+      <h2>Message from API : {message}</h2>
       <UiLogin />
       <PageTitle />
     </>
